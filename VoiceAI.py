77cf6f7f9a818679cddaba99bd5f7c250ab80063
Trung Nguyen <19868692@student.ltu.edu.au>
2021-09-26 12:45:35 +0700

    Added Voice Recognition to record button

--- VoiceAI.py
+++ VoiceAI.py
@@ -2,8 +2,9 @@ from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import ttk
 from tkinter import filedialog
-import speech_recognition
+import speech_recognition as sr
 import pyttsx3
+import time
 
 root = Tk()
 root.geometry('900x700')
@@ -12,7 +13,51 @@ render = ImageTk.PhotoImage(load)
 img = Label(root,image =render)
 img.place(x=0,y=0)
 
+def record():
+    robot_mouth = pyttsx3.init()
+    robot_ear = sr.Recognizer()
+    while True:
+     
+        with sr.Microphone() as mic: 
+                robot_ear.adjust_for_ambient_noise(mic)
+                print("AI Recognition: I'm listening")
+                audio = robot_ear.listen(mic)
+                time.sleep(1)
+        try:        
+                user = robot_ear.recognize_google(audio)  
+                user = user.lower()
+                print("User: " + user)
+        except sr.UnknownValueError:
+                
+                robot_brain = "Could you please repeat that"
+                robot_ear = sr.Recognizer()
+                user = ""
+                continue
+        
+        if "hello" in user:
+            robot_brain = "Hello user"
+        elif "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" in user:
+            robot_brain = user
+        elif "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + "5%" and "10%" and "25%" and "50%" and "75%" and "100%" and "minimum" and "minimum weight" and "bare weight" and "quarter Power" and "Half Power"and "Three Quarters Power" and "Full Power" in user:
+            robot_brain = user
+        elif "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + "5%" and "10%" and "25%" and "50%" and "75%" and "100%" and "minimum" and "minimum weight" and "bare weight" and "quarter Power" and "Half Power"and "Three Quarters Power" and "Full Power" + "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + "5%" and "10%" and "25%" and "50%" and "75%" and "100%" and "minimum" and "minimum weight" and "bare weight" and "quarter Power" and "Half Power"and "Three Quarters Power" and "Full Power" in user:
+            robot_brain = user
+        elif "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + "5%" and "10%" and "25%" and "50%" and "75%" and "100%" and "minimum" and "minimum weight" and "bare weight" and "quarter Power" and "Half Power"and "Three Quarters Power" and "Full Power" + "Aft" and "Fore, Port" and "Starboard" and "Port Quarter"and "Starboard Quarter" in user:
+            robot_brain = user
+        elif "alpha" and "bravo" and "charlie" and "1" and "2" and "3" and "A" and "B" and "C" and "Aft tug" and "Fore tug" and "Port tug" and "Starboard tug" and "Port quarter tug" and "Starboard quarter tug" + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + " and " + "pull" and "push" and "pull directly astern" and "all stop" and "move to pull" and "move to push" and "be ready to pull" and "be ready to push" and "move to" and "square up" + "Aft" and "Fore, Port" and "Starboard" and "Port Quarter"and "Starboard Quarter" in user:
+            robot_brain = user
+        elif "exit" in user:
+            robot_brain = "Bye!"
+            print("AI Recognition: " + robot_brain)
+            robot_mouth.say(robot_brain)
+            robot_mouth.runAndWait()
+            break
+        
+        print("AI Recognition: " + robot_brain)
+        robot_mouth.say(robot_brain)
+        robot_mouth.runAndWait()
 
+    
 def openNewWindow():
     newWindow = Toplevel(root)
     newWindow.title("FAQ")
@@ -62,11 +107,15 @@ def chatLog():
     txt.config(state=DISABLED)
     
 
+#record button
+img3 = PhotoImage(file = 'record.png')
+button3 = Button(root,image = img3,border=00, command = record)
+button3.place(x=10, y=10)
+
 #FAQ Button
 img1 = PhotoImage(file = 'button.png')
 button1 = Button(root,image = img1,border=2,command = openNewWindow, justify = 'right')
 button1.place(x=770, y=630)
-
 
 
 #Chat Log
@@ -74,11 +123,6 @@ img2 = PhotoImage(file = 'chatlog.png')
 button2 = Button(root,image = img2,border=0, command = chatLog)
 button2.place(x=100, y=10)
 
-
-#record button
-img3 = PhotoImage(file = 'record.png')
-button3 = Button(root,image = img3,border=00)
-button3.place(x=10, y=10)
 
 #FIle Browser Icon
 img4 = PhotoImage(file = 'folder.png')
